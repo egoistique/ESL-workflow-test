e2ebe074163c185ee7ef5748daa64ba46cb6ebf3
katya <katyamit5@gmail.com>
2023-10-19 14:05:36 +0300

    added input of values from the terminal
    Closes #1

--- test_app.c
+++ test_app.c
@@ -2,8 +2,13 @@
 #include "vector3d.h"
 
 int main() {
-    Vector3D v1 = {1.0, 2.0, 3.0};
-    Vector3D v2 = {4.0, 5.0, 6.0};
+    Vector3D v1, v2;
+
+    printf("Enter the coordinates of vector v1 (x y z): ");
+    scanf("%lf %lf %lf", &v1.x, &v1.y, &v1.z);
+
+    printf("Enter the coordinates of vector v2 (x y z): ");
+    scanf("%lf %lf %lf", &v2.x, &v2.y, &v2.z);
 
     Vector3D sum = vector3d_add(v1, v2);
     Vector3D difference = vector3d_subtract(v1, v2);
@@ -17,3 +22,4 @@ int main() {
 
     return 0;
 }
+
